7e9e1bf0773aca0910b124d8b1690d37c496bcf1
Samra01734 <Samramuqadasvu1@gmail.com>
2025-09-09 20:35:17 +0500

    10Th day

---
+++ 01advanceJs/mathUtils.js
@@ -0,0 +1,12 @@
+// Named exports
+export function add(a, b) {
+  return a + b;
+}
+export function subtract(a, b) {
+  return a - b;
+}
+
+// Default export
+export default function multiply(a, b) {
+  return a * b;
+}
