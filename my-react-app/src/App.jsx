c66142a30e74f506055262c768a0ddbe9403751b
Samra01734 <Samramuqadasvu1@gmail.com>
2025-09-24 22:23:11 +0500

    20Th day

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import React from 'react';
+import OIPIMAGE from "./OIP.jpeg";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+//********* Class based component */
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      items: [
+        { id: Date.now(), value: "Learn React JS", isDone: false },
+        { id: Date.now() + 1, value: "Build a Todo App", isDone: false }
+      ],
+      currentItem: ''
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  addItem(TodoValue) {
+    if (TodoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: TodoValue,
+        isDone: false
+      };
+      this.setState(prevState => ({
+        items: [...prevState.items, newItem],
+        newItem: ""
+      }));
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ newItem: e.target.value });
+  }
+
+  handleToggle(id) {
+    this.setState(prevState => ({
+      items: prevState.items.map(item =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    }));
+  }
+
+  handleDelete(id) {
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => item.id !== id)
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <img src={OIPIMAGE} width="100" height="100" className='logo' alt="OIP" />
+        <h1 className='App-title'>Sam** Todo App</h1>
+        <div className="container">
+          Add an Item..
+          <br />
+          <input
+            type="text"
+            className='input-text'
+            placeholder='Enter an item...'
+            value={this.state.newItem}
+            onChange={this.handleInputChange}
+          />
+          <br />
+          <button className='add-btn' onClick={() => this.addItem(this.state.newItem)}>
+            Add To-do
+          </button>
+          <div className="list">
+            <ul>
+              {this.state.items.map(item => (
+                <li key={item.id}>
+                  <input
+                    type='checkbox'
+                    checked={item.isDone}
+                    onChange={() => this.handleToggle(item.id)}
+                  />
+                  <span style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}>
+                    {item.value}
+                  </span>
+                  <button className='btn' onClick={() => this.handleDelete(item.id)}>
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
